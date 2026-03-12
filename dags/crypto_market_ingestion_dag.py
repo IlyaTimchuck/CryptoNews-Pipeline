64bf39052917e6d9bd8736fe30e67be7aaca0a2c
Илья <tim3uck@gmail.com>
2026-03-12 20:10:26 +0300

    feat: implement newsdata ingestion: - Integrate NewsData.io API with category-based keyword filtering - Implement 'land_news_data' with dynamic sub-directory partitioning - Create 'news_key_words' Airflow Variable using JSON-deserialization - Optimize TaskFlow dependencies for parallel Market and News ingestion - Implement AirflowSkipException to handle empty API response states

--- dags/crypto_market_ingestion_dag.py
+++ dags/crypto_market_ingestion_dag.py
@@ -1,5 +1,6 @@
-from airflow.sdk import dag, task
+from airflow.decorators import dag, task
 from airflow.models import Variable
+from airflow.exceptions import AirflowSkipException
 from datetime import datetime
 from typing import List, Dict, Any
 import requests
@@ -39,14 +40,63 @@ def crypto_market_ingestion_dag():
     @task
     def land_coins_data(coins_data: List[Dict[str, Any]]) -> str:
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
-        file_path = f"include/raw_coins_data_{timestamp}.json"
-        with open(f"include/raw_data/raw_coins_data_{timestamp}.json", "w") as f:
+        base_path = "include/raw_data/raw_coins_data"
+        file_path = f"{base_path}/raw_coins_{timestamp}.json"
+        
+        os.makedirs(base_path, exist_ok=True)
+
+        with open(file_path, "w") as f:
             json.dump(coins_data, f, indent=4)
+
         return file_path
-    
-    fetching_coins_data = fetch_top_coins_data()
-    landing_coins_data = land_coins_data(fetching_coins_data)
-    fetching_coins_data >> landing_coins_data
+
+    @task
+    def fetch_news_data(coins_data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        url = "https://newsdata.io/api/1/latest"
+        key_words = Variable.get("news_key_words", default_var={}, deserialize_json=True)
+        key_words["coin_names"] = [coin["name"] for coin in coins_data[:15]]
+
+        params = {
+            "apikey": os.getenv("NEWSDATA_API_KEY"),
+            "language": "en",
+            "size": 10
+            }
+
+        news_data = {}
+        for category in key_words:
+            params["q"] = " OR ".join(key_words[category])
+            if len(params["q"]) < 450:
+                response = requests.get(url=url, params=params)
+                articles = response.json().get("results", [])
+                news_data[category] = articles 
+            else:
+                print(f"Skipping {category}: Query too long")
+
+        if not news_data:
+            raise AirflowSkipException("No news found for any category.")
+        
+        return news_data
+
+    @task
+    def land_news_data(news_data: dict):
+        if not news_data:
+            raise AirflowSkipException("The variable news_data is empty.")
+
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
+        base_path = "include/raw_data/raw_news_data"
+
+        os.makedirs(base_path, exist_ok=True)
+
+        for category, articles in news_data.items(): 
+            with open(f"{base_path}/raw_news_{category}_{timestamp}.json", "w") as f:
+                json.dump(articles, f, indent=4)
+
+
+    raw_coins = fetch_top_coins_data()
+    land_coins = land_coins_data(raw_coins)
+    raw_news = fetch_news_data(raw_coins)
+    land_news = land_news_data(raw_news)
+
 
 
 crypto_market_ingestion_dag()
